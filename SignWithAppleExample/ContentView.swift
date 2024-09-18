ab7f903563f468eddef6d28703a889769bd74057
Snooopyman <armando.snoop@gmail.com>
2024-09-18 19:46:33 +0200

    Initial Commit

---
+++ SignWithAppleExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SignWithAppleExample
+//
+//  Created by Armando Cáceres on 18/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SignWithAppleExample/SignWithAppleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignWithAppleExampleApp.swift
+//  SignWithAppleExample
+//
+//  Created by Armando Cáceres on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SignWithAppleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
